f2283582d3dd1896549546c54fa7fe70d9cd899a
Aren2020 <aren.allahverdyan@gmail.com>
2024-09-08 11:51:11 +0300

    models for text

--- runningtext/text/models.py
+++ runningtext/text/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+
+def validate_video_extension(value):
+    if not value.name.endswith(('.mp4')):
+        raise ValidationError("Only video files are allowed.")
+
+class Text(models.Model):
+    text = models.CharField(max_length = 50)
+    video = models.FileField(upload_to = 'video', validators = [validate_video_extension])
+    created_at = models.DateField(auto_now_add = True)
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        # For admin panel
+        ordering = ['-created_at']
+        indexes = [
+            models.Index(fields = ['-created_at']),
+        ]
